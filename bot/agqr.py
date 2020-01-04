d76dce4b8f706ba75c1774b1e5c4e0d11f4b4a04
Nep <spica@starry.blue>
2020-01-04 10:42:27 +0900

    AgqrProgram をデータクラス化した

--- bot/agqr.py
+++ bot/agqr.py
@@ -1,12 +1,14 @@
+from __future__ import annotations
+
 import asyncio
 import re
 import time
 import urllib.parse
-from asyncio import Task
+from dataclasses import dataclass
 from datetime import datetime
 from subprocess import PIPE, Popen
 from threading import Thread
-from typing import Any, Dict, Optional
+from typing import Optional
 
 import aiohttp
 from discord import (Embed, FFmpegPCMAudio, PCMVolumeTransformer, Streaming,
@@ -27,7 +29,6 @@ class AgqrPlayer(commands.Cog):
 
         if App.config.text_channel_id:
             self.text_channel = App.client.get_channel(App.config.text_channel_id)
-            await self.text_channel.purge(check=lambda x: x.author == App.client.user)
         self.voice_channel = App.client.get_channel(App.config.voice_channel_id)
 
         member = self.voice_channel.guild.get_member(App.client.user.id)
@@ -54,7 +55,63 @@ class AgqrPlayer(commands.Cog):
 
             time.sleep(5)
 
-    async def get_on_air(self) -> Dict[str, Any]:
+    async def update(self):
+        last_program_name = None
+        last_message = None
+
+        async for message in self.text_channel.history(limit=10):
+            if message.author == App.client.user:
+                last_message = message
+                break
+
+        while App.client.loop.is_running():
+            program = await AgqrProgram.get_on_air()
+
+            if program.name != last_program_name:
+                if self.text_channel:
+                    async with self.text_channel.typing():
+                        embed = Embed(
+                            title=program.name,
+                            description=program.description,
+                            url=program.link_url or "https://www.agqr.jp",
+                            color=0xe30067,
+                            timestamp=datetime.utcnow()
+                        )
+                        embed.set_author(
+                            name=program.personality if program.personality else "文化放送",
+                            url=program.link_url or "https://www.agqr.jp"
+                        )
+
+                        if last_message:
+                            await last_message.edit(embed=embed)
+                        else:
+                            last_message = await self.text_channel.send(embed=embed)
+
+                await App.client.change_presence(
+                    activity=Streaming(
+                        name=program.name,
+                        url="https://twitch.tv/slashnephy"
+                    )
+                )
+
+                last_program_name = program.name
+
+            await asyncio.sleep(30)
+
+@dataclass
+class AgqrProgram:
+    name: str
+    img_url: Optional[str]
+    link_url: Optional[str]
+    description: Optional[str]
+    personality: Optional[str]
+    ad_img_url: Optional[str]
+    ad_link_url: Optional[str]
+    music_title: Optional[str]
+    music_artist: Optional[str]
+
+    @classmethod
+    async def get_on_air(cls) -> AgqrProgram:
         async with aiohttp.ClientSession() as session:
             headers = {
                 "Accept": "*/*",
@@ -65,57 +122,21 @@ class AgqrPlayer(commands.Cog):
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36"
             }
 
-            async with session.get("http://www.uniqueradio.jp/aandg", headers=headers) as response:
+            async with session.get("https://www.uniqueradio.jp/aandg", headers=headers) as response:
+                text = await response.text()
                 t = [
                     urllib.parse.unquote(re.sub("^.+?'(.*)';$", r"\1", line))
-                    for line in (await response.text()).splitlines()
+                    for line in text.splitlines()
                 ]
 
-                return {
-                    "name": t[0],
-                    "banner_url": t[1] if t[1] else None,
-                    "url": t[2] if t[2] else None,
-                    "description": re.sub("<.+?>", "", t[3].replace("<br>", "\n")) if t[3] else None,
-                    "cast": t[4] if t[4] else None,
-                    "ad_img_url": t[5] if t[5] else None,
-                    "ad_url": t[6] if t[6] else None,
-                    "music_title": t[7] if t[7] else None,
-                    "music_artist": t[8] if t[8] else None
-                }
-
-    async def update(self):
-        last_program_name = None
-        last_message = None
-
-        while App.client.loop.is_running():
-            program = await self.get_on_air()
-
-            if program["name"] != last_program_name:
-                if self.text_channel:
-                    async with self.text_channel.typing():
-                        embed = Embed(
-                            title=program["name"],
-                            description=program["description"],
-                            url=program["url"] or "https://www.agqr.jp",
-                            color=0xe30067,
-                        )
-                        embed.set_author(
-                            name=program["cast"] if program["cast"] else program["name"],
-                            url=program["url"] or "https://www.agqr.jp"
-                        )
-
-                        if last_message:
-                            last_message = await last_message.edit(embed=embed)
-                        else:
-                            last_message = await self.text_channel.send(embed=embed)
-
-                await App.client.change_presence(
-                    activity=Streaming(
-                        name=program["name"] + " (超A&G+)",
-                        url="https://twitch.tv/slashnephy"
-                    )
+                return AgqrProgram(
+                    name=t[0],
+                    img_url=t[1] if t[1] else None,
+                    link_url=t[2] if t[2] else None,
+                    description=re.sub("<.+?>", "", t[3].replace("<br>", "\n")) if t[3] else None,
+                    personality=t[4] if t[4] else None,
+                    ad_img_url=t[5] if t[5] else None,
+                    ad_link_url=t[6] if t[6] else None,
+                    music_title=t[7] if t[7] else None,
+                    music_artist=t[8] if t[8] else None
                 )
-
-                last_program_name = program["name"]
-
-            await asyncio.sleep(30)
